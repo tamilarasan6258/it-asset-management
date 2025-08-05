1d8a511093af549e4ced8f7fffd08d5c6290b013
tamilarasan6258 <sstamilarasan14@gmail.com>
2025-08-05 16:07:18 +0530

    fix error

---
+++ Frontend/it-asset-management/src/app/environment/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    asset_apiBaseUrl:"http://localhost:5000/api/assets",
+    assignment_apiBaseUrl:"http://localhost:5000/api/assignments",
+    auth_apiBaseUrl:"http://localhost:5000/api/auth",
+    request_apiBaseUrl:"http://localhost:5000/api/requests",
+    user_apiBaseUrl:"http://localhost:5000/api/users"
+}
